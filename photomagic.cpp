515e29b95ef22ef5788f91ea2361eddb66f6b7b8
map5 <markpriscella@gmail.com>
2020-08-29 18:24:14 -0400

    changes to photomagic so no window opens

--- photomagic.cpp
+++ photomagic.cpp
@@ -45,8 +45,8 @@ int main(int argc, const char * argv[])
 	}
 
 	/* create the two windows */
-	sf::RenderWindow window1(sf::VideoMode(size.x, size.y), argv[1]);
-	sf::RenderWindow window2(sf::VideoMode(size.x, size.y), argv[2]);
+	//sf::RenderWindow window1(sf::VideoMode(size.x, size.y), argv[1]);
+	//sf::RenderWindow window2(sf::VideoMode(size.x, size.y), argv[2]);
 
 	/* create and load texture1 from image1 */
 	sf::Texture texture1;
@@ -64,28 +64,28 @@ int main(int argc, const char * argv[])
 	sprite2.setTexture(texture2);
 	
 
-	while (window1.isOpen() && window2.isOpen()) {
+	// while (window1.isOpen() && window2.isOpen()) {
 
-		sf::Event event;
+	// 	sf::Event event;
 
-		while (window1.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window1.close();
-		}
+	// 	while (window1.pollEvent(event)) {
+	// 		if (event.type == sf::Event::Closed)
+	// 			window1.close();
+	// 	}
 
-		while (window2.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window2.close();
-		}
+	// 	while (window2.pollEvent(event)) {
+	// 		if (event.type == sf::Event::Closed)
+	// 			window2.close();
+	// 	}
 
-		window1.clear();
-		window1.draw(sprite1);
-		window1.display();
+	// 	window1.clear();
+	// 	window1.draw(sprite1);
+	// 	window1.display();
 
-		window2.clear();
-		window2.draw(sprite2);
-		window2.display();
- 	}
+	// 	window2.clear();
+	// 	window2.draw(sprite2);
+	// 	window2.display();
+ 	// }
 
 	// fredm: saving a PNG segfaults for me, though it does properly
 	//   write the file
